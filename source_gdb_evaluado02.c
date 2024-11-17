f922b810761f2e6892ff821ba120daa01c94abe9
Luisdavid Colina <luisdavid.colina@gmail.com>
2024-11-17 18:26:56 -0400

    test

--- source_gdb_evaluado02.c
+++ source_gdb_evaluado02.c
@@ -3,76 +3,96 @@
 
 typedef struct Nodo
 {
-	char data;
-	struct Nodo *next;
+    char data;
+    struct Nodo *next;
 } Nodo;
+
 Nodo *first, *last;
 
 void Agregar_elemento(char data)
 {
-	Nodo *Auxiliar = malloc(sizeof(Nodo));
-	if (first == NULL)
-	{
-		Auxiliar->data = data;
-		Auxiliar->next = NULL;
-		first = Auxiliar;
-	}
-	else
-	{
-		last = first;
-		while (last->next != NULL)
-		{
-			last = last->next;
-		}
-		last->next = Auxiliar;
-		Auxiliar->data = data;
-		Auxiliar->next = NULL;
-	}
+    Nodo *Auxiliar = malloc(sizeof(Nodo));
+    if (Auxiliar == NULL)
+    {
+        printf("Error al asignar memoria.\n");
+        exit(1);
+    }
+
+    Auxiliar->data = data;
+    Auxiliar->next = NULL;
+
+    if (first == NULL)
+    {
+        first = Auxiliar;
+    }
+    else
+    {
+        last = first;
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = Auxiliar;
+    }
 }
 
 void printword(char *string)
 {
-	printf("%s", string);
-	return;
+    printf("%s", string);
 }
 
 void Imprimir_cedula()
 {
-	last = first;
-	while (last != NULL)
-	{
-		printf("%c", last->data);
-		last = last->next;
-	}
-	printf("\n");
+    last = first;
+    while (last != NULL)
+    {
+        printf("%c", last->data);
+        last = last->next;
+    }
+    printf("\n");
+}
+
+void liberar_lista()
+{
+    Nodo *temp;
+    while (first != NULL)
+    {
+        temp = first;
+        first = first->next;
+        free(temp);
+    }
 }
 
 int main()
 {
-
-	first = NULL;
+    first = NULL;
     last = NULL;
 
+    char i = '\0';
+    char string[100];
+    char *frase = "Escriba su nombre:";
 
-	char i = '\0';
-	char string[100];
-	char *frase = "Escriba su nombre:";
+    printf("%s ", frase);
+    scanf("%s", string); // Lee el nombre
+    printword(string);
+    printf("\nIngrese su C.I (termina con Enter): \n");
 
-	printf("%s", frase);
-	scanf("%s", &string);
-	printword(string);
-	printf("Ingrese su C.I: \n");
-
-	while (1)
+    // Usar un espacio antes de %c en scanf para ignorar caracteres residuales
+    while (1)
     {
         scanf(" %c", &i);
-        if (i == '\n') 
+        if (i == '\n') // Finalizar cuando se presione Enter
         {
             break;
         }
         Agregar_elemento(i);
     }
 
-	Imprimir_cedula();
-	return 1;
+    // Imprime los datos de la lista
+    printf("C.I ingresada: ");
+    Imprimir_cedula();
+
+    // Libera la memoria utilizada
+    liberar_lista();
+    return 0;
 }
